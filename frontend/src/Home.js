ca104b3408b34379ac8b4ba1a68ac34585b1c33d
arusa-shah <arusashah786@gmail.com>
2025-08-10 02:36:20 +0530

    fix: update API endpoint for email summary submission

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -130,7 +130,7 @@ function Home() {
     }
   
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/email/", {
+      const response = await fetch("http://127.0.0.1:8000/api/email-summary/", {
         method: "POST",
         headers: {
           'Content-Type': 'application/json',
